804fbed1b2e190a88dc2c6f75cd3e8594910a2b6
gamer-dhairya <92428258+gamer-dhairya@users.noreply.github.com>
2022-01-01 16:03:19 +0530

    Add files via upload

---
+++ cannon.js
@@ -0,0 +1,27 @@
+class Cannon{
+
+    constructor(x,y,width,height,angle){
+        this.x=x;
+        this.y=y;
+        this.width=width;
+        this.height=height;
+        this.angle=angle;
+
+        this.cannon_image=loadImage("assets/canon.png");
+        this.cannonBase=loadImage("assets/cannonBase.png");
+
+    }
+
+    display(){
+        push();
+
+        imageMode(CENTER);
+        image( this.cannon_image,this.x,this.y,this.width,this.height);
+        
+        pop();
+
+        image(this.cannonBase,70,20,200,200);
+        nofill();
+        
+    }
+}
